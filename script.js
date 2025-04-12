726f8523c5a7e57905c5a8029cf703e64c36f02a
Tanya Hrynda <135536194+tanya7783@users.noreply.github.com>
2025-04-12 12:59:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Перевірка наявності елемента бургер-меню
-    const burgerButton = document.querySelector('.burger-button');
-    const nav = document.querySelector('nav#mainNav');
-
-    if (burgerButton && nav) {
-        // Додаємо обробник події для бургер-меню
-        burgerButton.addEventListener('click', () => {
-            nav.classList.toggle('active');
-            burgerButton.classList.toggle('active');
-        });
-    } else {
-        console.log("Елементи не знайдені: .burger-button або #mainNav.");
-    }
-
     // Обробка вибору відповіді
     document.querySelectorAll('.answer').forEach(answer => {
         answer.addEventListener('click', function() {
@@ -22,52 +8,57 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Обробка відправки форми
-    document.getElementById('testForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        let score = 0;
-        document.querySelectorAll('.question').forEach(question => {
-            const selectedAnswer = question.querySelector('.answer.selected');
-            const answers = question.querySelectorAll('.answer');
-            const correctAnswerText = question.querySelector('.correct-answer');
+    // Перевірка та обробка відправки форми для тесту
+    const testForm = document.getElementById('testForm');
+    if (testForm) {
+        testForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            let score = 0;
+            document.querySelectorAll('.question').forEach(question => {
+                const selectedAnswer = question.querySelector('.answer.selected');
+                const answers = question.querySelectorAll('.answer');
+                const correctAnswerText = question.querySelector('.correct-answer');
 
-            // Очищення старих класів
-            answers.forEach(answer => {
-                answer.classList.remove('correct', 'incorrect');
+                // Очищення старих класів
+                answers.forEach(answer => {
+                    answer.classList.remove('correct', 'incorrect');
+                });
+
+                if (selectedAnswer) {
+                    if (selectedAnswer.getAttribute('data-correct') === 'true') {
+                        score++;
+                        selectedAnswer.classList.add('correct');
+                    } else {
+                        selectedAnswer.classList.add('incorrect');
+                    }
+                }
+
+                if (correctAnswerText) {
+                    correctAnswerText.style.display = 'block';
+                }
+
+                // Відображення правильних відповідей
+                answers.forEach(answer => {
+                    if (answer.getAttribute('data-correct') === 'true') {
+                        answer.classList.add('correct');
+                    }
+                });
             });
 
-            if (selectedAnswer) {
-                if (selectedAnswer.getAttribute('data-correct') === 'true') {
-                    score++;
-                    selectedAnswer.classList.add('correct');
-                } else {
-                    selectedAnswer.classList.add('incorrect');
-                }
-            }
+            // Виведення результату
+            const resultDiv = document.createElement('div');
+            resultDiv.classList.add('result');
+            resultDiv.innerHTML = `Ваш результат: ${score} з ${document.querySelectorAll('.question').length}`;
+            document.querySelector('section').appendChild(resultDiv);
+            setTimeout(() => {
+                resultDiv.classList.add('fade-in');
+            }, 100);
 
-            if (correctAnswerText) {
-                correctAnswerText.style.display = 'block';
-            }
-
-            // Відображення правильних відповідей
-            answers.forEach(answer => {
-                if (answer.getAttribute('data-correct') === 'true') {
-                    answer.classList.add('correct');
-                }
-            });
+            startTimer(5 * 60);  // Запуск таймера на 5 хвилин
         });
-
-        // Виведення результату
-        const resultDiv = document.createElement('div');
-        resultDiv.classList.add('result');
-        resultDiv.innerHTML = `Ваш результат: ${score} з ${document.querySelectorAll('.question').length}`;
-        document.querySelector('section').appendChild(resultDiv);
-        setTimeout(() => {
-            resultDiv.classList.add('fade-in');
-        }, 100);
-
-        startTimer(5 * 60);  // Запуск таймера на 5 хвилин
-    });
+    } else {
+        console.log("Елемент 'testForm' не знайдений.");
+    }
 
     let timerInterval;
     // Функція для запуску таймера
@@ -92,31 +83,54 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Обробка форми контактів
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
+    const contactForm = document.getElementById('contactForm');
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const message = document.getElementById('message').value;
 
-        if (name && email && message) {
-            const successMessage = document.getElementById('successMessage');
-            successMessage.textContent = 'Ваше повідомлення успішно надіслано!';
-            successMessage.style.display = 'block';
-            document.getElementById('contactForm').reset();
-        } else {
-            alert('Будь ласка, заповніть всі поля.');
-        }
-    });
+            if (name && email && message) {
+                const successMessage = document.getElementById('successMessage');
+                successMessage.textContent = 'Ваше повідомлення успішно надіслано!';
+                successMessage.style.display = 'block';
+                contactForm.reset();
+            } else {
+                alert('Будь ласка, заповніть всі поля.');
+            }
+        });
+    } else {
+        console.log("Елемент 'contactForm' не знайдений.");
+    }
 
-    // Обробка мобільного меню
+    // Отримуємо елементи меню та кнопку бургер-меню
+    const burgerButton = document.querySelector('.burger-button');
+    const sidebarMenu = document.querySelector('nav ul');
+
+    // Додаємо обробник подій на клік по кнопці бургер-меню
+    if (burgerButton && sidebarMenu) {
+        burgerButton.addEventListener('click', () => {
+            // Перемикаємо клас для відкриття/закриття меню
+            sidebarMenu.classList.toggle('active');
+        });
+    } else {
+        console.log("Бургер-меню або бічне меню не знайдено.");
+    }
+
+    // Обробка бургер-меню для мобільної версії
     const mobileMenuButton = document.getElementById('mobile-menu');
     if (mobileMenuButton) {
         mobileMenuButton.addEventListener('click', function() {
             const nav = document.getElementById('mainNav');
-            nav.classList.toggle('active');
-            mobileMenuButton.classList.toggle('active');
+            if (nav) {
+                nav.classList.toggle('active'); // Перемикає клас "active" для меню
+                mobileMenuButton.classList.toggle('active'); // Перемикає клас "active" для бургер-меню
+            } else {
+                console.log("Меню не знайдено.");
+            }
         });
     } else {
-        console.log("Елемент '#mobile-menu' не знайдений.");
+        console.log("Кнопка мобільного меню не знайдена.");
     }
 });
